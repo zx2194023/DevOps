d846ae4208d804d8469408b297ba0b7ecba7dad9
COLA <zx21940@gmail.com>
2017-11-25 15:05:03 +0800

    modified main.go. and test.go to calculate div

--- main.go
+++ main.go
@@ -1,35 +1,62 @@
 package main
 
 import (
-    "net/http"
-    "strconv"
-
-    "github.com/labstack/echo"
+   "net/http"
+  
+   "github.com/labstack/echo"
+  "strconv"
+  "errors"
 )
 
 func main() {
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+   e := echo.New()
+   e.GET("/", func(c echo.Context) error {
+       return c.String(http.StatusOK, "Hello, World!")
+   })
 
-    e.GET("/add/:num1/:num2", func(c echo.Context) error {
-        num1Str := c.Param("num1")
-        num2Str := c.Param("num2")
-        num1, err := strconv.Atoi(num1Str)
-        if err != nil {
-            return c.String(http.StatusUnprocessableEntity, err.Error())
-        }
-        num2, err := strconv.Atoi(num2Str)
-        if err != nil {
-            return c.String(http.StatusUnprocessableEntity, err.Error())
-        }
-        result := Add(num1, num2)
-        return c.String(http.StatusOK, strconv.Itoa(result))
-    })
-    e.Logger.Fatal(e.Start(":1324"))
+   e.GET("/add/:num1/:num2", func(c echo.Context) error {
+     num1Str := c.Param("num1")
+     num2Str := c.Param("num2")
+     num1, err := strconv.Atoi(num1Str)
+     if err != nil {
+        return c.String(http.StatusUnprocessableEntity, err.Error())
+     }
+     num2, err := strconv.Atoi(num2Str)
+     if err != nil {
+        return c.String(http.StatusUnprocessableEntity, err.Error())
+     }
+     result := Add(num1, num2)
+     return c.String(http.StatusOK, strconv.Itoa(result))
+  })
+
+  e.GET("/div/:num1/:num2", func(c echo.Context) error {
+     num1Str := c.Param("num1")
+     num2Str := c.Param("num2")
+     num1, err := strconv.Atoi(num1Str)
+     if err != nil {
+        return c.String(http.StatusUnprocessableEntity, err.Error())
+     }
+     num2, err := strconv.Atoi(num2Str)
+     if err != nil {
+        return c.String(http.StatusUnprocessableEntity, err.Error())
+     }
+     result, err := Div(num1, num2)
+     if err != nil {
+        return c.String(http.StatusUnprocessableEntity, err.Error())
+     }
+     return c.String(http.StatusOK, strconv.Itoa(result))
+  })
+
+  e.Logger.Fatal(e.Start(":1325"))
 }
 
 func Add(num1, num2 int) int {
-    return num1 + num2
+  return num1 + num2
+}
+
+func Div(num1, num2 int) (int, error) {
+  if num2 == 0{
+     return 0, errors.New("number 2 cnannot be 0")
+  }
+  return num1 / num2, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-        "testing"
+       "testing"
 )
 
 func TestAdd(t *testing.T) {
-        result := Add(1, 3)
-        if result != 4 {
-                t.Errorf("should be 4 but %d", result)
-        }
+       result := Add(1, 3)
+       if result != 4 {
+               t.Errorf("should be 4 but %d", result)
+       }
+}
+
+func TestDiv(t *testing.T) {
+       result,err := Div(4, 2)
+       if err!=nil{
+               t.Error(err.Error())
+       }
+       if result != 2 {
+               t.Errorf("should be 4 but %d", result)
+       }
+}
+
+func TestDivZero(t *testing.T) {
+       _, err := Div(4, 0)
+       if err==nil{
+               t.Error("cannot pass")
+       }
 }
